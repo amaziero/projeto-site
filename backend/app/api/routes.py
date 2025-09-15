0687791a2475614c919fd8bc7976eb937217952f
amaziero <alison_lens@hotmail.com>
2025-09-15 19:36:47 -0300

    post com validacao de tipo de arquivo

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+# http://127.0.0.1:8000/docs
+
+from fastapi import APIRouter, UploadFile, File, HTTPException
 
 router = APIRouter(prefix="/v1", tags=["v1"])
 
@@ -12,5 +14,27 @@ def health():
 
 
 @router.post("/upload", tags=["Upload"])
-async def upload_file(nome: str, idade: int):
-    return {"message": f"Nome {nome} e idade {idade} recebidos com sucesso!"}
+async def upload_file(file: UploadFile = File(...)):
+    
+    # chegar se a extensão do arquivo é .pdf
+    if not file.filename.endswith(".pdf"):
+        raise HTTPException(status_code=400, detail="Aqui se processa apenas PDF.")
+    
+    if file.content_type != 'application/pdf':
+        raise HTTPException(
+            status_code=400,
+            detail="Tipo de arquivo invalido. Aqui so se processa pdf."
+        )
+    
+    content = await file.read(4)
+    if content != b'%PDF':
+        raise HTTPException(
+            status_code=400,
+            detail="Tipo de arquivo invalido. Aqui so se processa pdf."
+        )
+    
+    await file.seek(0)
+
+    return {
+        "message": f"Arquivo {file.filename} recebido com sucesso."
+    }
